0c7c828f418a78030b6253a0ec20bcb7fc167f1b
YevdokimovOleksandr <124283297+YevdokimovOleksandr@users.noreply.github.com>
2023-02-16 18:44:56 +0200

    first tests 15 modul

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,1 +1,17 @@
-new file
+import pytest
+
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == 'Oleksandr'
+
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == 'Yevdokimov' 
